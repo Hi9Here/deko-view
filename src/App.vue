0103fd87dd88c58919f2965cfb441a8edb16a1c3
woisme <mywoisme@gmail.com>
2018-05-24 17:35:55 +0100

    fixed drawer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <v-app>
+      <v-navigation-drawer clipped temporary app v-model="drawer">
+        <v-list>
+          <v-list-tile v-for="item in menuItems" :key="item.title">
+            <v-list-tile-action>
+              <v-icon>{{ item.icon}}</v-icon>
+            </v-list-tile-action>
+              <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
     <v-toolbar dark class="red darken-2">
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Deko Fit</v-toolbar-title>
+      <v-toolbar-title>
+        <router-link to="/" tag="span" style="cursor: pointer">Deko Fit</router-link>
+        </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
         <v-btn flat v-for="item in menuItems" :key="item.title">
@@ -13,16 +25,6 @@
     <main>
       <router-view></router-view>
     </main>
-      <v-navigation-drawer v-model="drawer">
-        <v-list>
-          <v-list-tile v-for="item in menuItems" :key="item.title">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon}}</v-icon>
-            </v-list-tile-action>
-              <v-list-tile-content>{{ item.title }}</v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-navigation-drawer>
   </v-app>
 </template>
 
@@ -32,11 +34,11 @@ export default {
     return {
       drawer: false,
       menuItems: [
-        { icon: 'fitness_center', title: 'Exercises'},
-        { icon: 'room', title: 'New Exercise'},
-        { icon: 'person', title: 'Profile'},
-        { icon: 'face', title: 'Sign Up'},
-        { icon: 'lock_open', title: 'Sign In'}
+        { icon: 'fitness_center', title: 'Exercises', link: '/exercises'},
+        { icon: 'room', title: 'New Exercise', link: 'exercises/new'},
+        { icon: 'person', title: 'Profile', link: '/profile'},
+        { icon: 'face', title: 'Sign Up', link: 'signup'},
+        { icon: 'lock_open', title: 'Sign In', link: 'signin'}
       ]
     }
   }
